315735a197ec5d504fc2c8a10389de98e2854402
Chakrit Wichian <chakrit@oozou.com>
2012-10-30 12:17:33 +0700

    Minor refactoring.

--- index.js
+++ index.js
@@ -5,20 +5,19 @@ var URL = 'http://harveststatus.com/status.json'
 var request = require('request')
   , colors = require('colors') // infect String.prototype
   , log = console.log
-  , spinCheck = function(e) {
-    if (e) return log(e.message || e.stack)
 
+  , spinCheck = function() {
     request(URL, function(e, resp, body) {
+      if (e) return log(e.message || e.stack)
       body = JSON.parse(body)
 
       var status = body.status
-      if (status === 'up') status = status.green
-      else status = status.red
+      status = (status === 'up') ? status.green : status.red
 
       log(status + '\t' + body.last_response_time)
       setTimeout(spinCheck, INTERVAL)
     })
   }
 
-spinCheck()
+module.exports = spinCheck()
 
