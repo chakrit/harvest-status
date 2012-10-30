449a6e058fdb17a4048435d406c3765f12eae9d3
Chakrit Wichian <chakrit@oozou.com>
2012-10-30 11:54:20 +0700

    Add some colors to log output.

--- index.js
+++ index.js
@@ -3,13 +3,19 @@ var URL = 'http://harveststatus.com/status.json'
   , INTERVAL = 1000
 
 var request = require('request')
+  , colors = require('colors') // infect String.prototype
   , log = console.log
   , spinCheck = function(e) {
     if (e) return log(e.message || e.stack)
 
     request(URL, function(e, resp, body) {
       body = JSON.parse(body)
-      log(body.status + '\t' + body.last_response_time)
+
+      var status = body.status
+      if (status === 'up') status = status.green
+      else status = status.red
+
+      log(status + '\t' + body.last_response_time)
       setTimeout(spinCheck, INTERVAL)
     })
   }
